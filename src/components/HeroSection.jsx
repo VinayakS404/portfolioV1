028562058ab01818503fc0ff67b0a81a4d84a3ba
Vinayak S <oreva.vinayak@gmail.com>
2026-02-18 02:26:28 +0530

    feat: add icons

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,17 @@
 import mapIcon from "../assets/maps.png";
+import githubIcon from "../assets/github.png";
+import linkedinIcon from "../assets/linkedin.png";
+import mailIcon from "../assets/email.png";
 function HeroSection() {
   return (
     <>
       <div className="h-screen  flex flex-col  items-center text-white">
-        <div className="   h-[30%] w-full flex justify-end items-start text-2xl gap-10 mt-[calc(100%-97%)] mr-50">
-          <p>home</p>
-          <p>About</p> <p>Contact</p>
+        <div className="   h-[30%] w-full flex justify-end items-start text-xl gap-10 mt-[calc(100%-97%)] mr-50">
+          <p>Home</p>
+          <p>Skills</p>
+          <p>About</p>
+          <p>Projects</p>
+          <p>Contact</p>
         </div>
         <div className=" h-[40%] flex flex-col items-center justify-center gap-3">
           <div className="flex items-center justify-center gap-1">
@@ -13,20 +19,25 @@ function HeroSection() {
             <p className=" text-lg">available for work</p>
           </div>
           <div className="flex flex-col items-center justify-center gap-7">
-            <p className="font-['Anton'] text-7xl w-160 text-center">
+            <p className="font-['Anton'] text-8xl w-230 text-center">
               <span className="text-blue-500">Frontend</span> Dev Leveling Up to{" "}
               <span className="text-blue-500">Full-Stack</span>
             </p>
-            <p className="text-center text-2xl">
+            <p className="text-center text-2xl text-gray-400">
               I’m <span className="text-blue-500 font-bold">Vinayak S</span>,
               building web experiences while pursuing my BCA
             </p>
           </div>
         </div>
-        <div className="  h-[30%] flex items-end justify-center  text-2xl gap-30 mb-[calc(100%-97%)]">
+        <div className="  h-[30%] flex items-end justify-center  text-2xl gap-8 mb-[calc(100%-97%)]">
           <div className="flex items-center justify-center gap-1">
-            <img src={mapIcon} alt="pin-icon" className="h-5" />
+            <img src={mapIcon} alt="pin icon" className="h-5" />
             <p>Kerala, India</p>
+          </div>{" "}
+          <div className="flex gap-4">
+            <img src={githubIcon} alt="github icon" className="h-9" />
+            <img src={linkedinIcon} alt="github icon" className="h-9" />
+            <img src={mailIcon} alt="github icon" className="h-9" />
           </div>
           <a href="">
             <p>Download CV</p>
