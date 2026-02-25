5bd420a5a0a05e750b2be4e81be6f54f04007547
Vinayak S <oreva.vinayak@gmail.com>
2026-02-26 02:11:47 +0530

    feat: working on responsive ui

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,15 +5,15 @@ import mailIcon from "../assets/email.png";
 import NavBar from "./NavBar";
 function HeroSection() {
   return (
-    <div className="h-screen max-h-200  flex flex-col  items-center text-white">
+    <div className=" h-screen max-h-200  flex flex-col  items-center text-white px-8">
       <NavBar />
-      <div className=" h-[40%] flex flex-col items-center justify-center gap-3">
+      <div className=" h-[60%] flex flex-col items-center justify-center gap-3">
         <div className="flex items-center justify-center gap-1">
-          <span className="h-3 w-3  bg-[#55ff00] rounded-full translate-y-0.5"></span>
-          <p className=" text- text-sm  md:text-md lg:text-lg">available for work</p>
+          <span className="h-2 w-2 md:h-2.5 md:w-2.5 lg:h-3 lg:w-3  bg-[#55ff00] rounded-full translate-y-0.5"></span>
+          <p className="  text-sm  md:text-md lg:text-lg">available for work</p>
         </div>
         <div className="flex flex-col items-center justify-center gap-7">
-          <p className="font-['Anton'] text-3xl md:text-5xl lg:text-7xl w-full md:w-160  text-center">
+          <p className="font-['Anton'] text-6xl lg:text-7xl w-full md:w-160  text-center">
             <span className="text-blue-500">Frontend&nbsp;</span>Dev Leveling Up
             to
             <span className="text-blue-500">&nbsp;Full-Stack</span>
@@ -25,31 +25,52 @@ function HeroSection() {
           </p>
         </div>
       </div>
-      <div className="  h-[30%] flex items-end justify-center  text-2xl gap-8 mb-[calc(100%-97%)]">
-        <div className="flex items-center justify-center gap-1">
-          <img src={mapIcon} alt="pin icon" className="h-5" />
-          <p>Kerala, India</p>
+      <div className="  h-[30%]  flex flex-col sm:flex-row  items-center sm:items-end justify-center text-2xl gap-3 md:gap-8 mb-[calc(100%-97%)] ">
+        <div className="hidden md:flex items-center justify-center gap-1">
+          <img src={mapIcon} alt="pin icon" className=" h-4 md:h-5 " />
+          <p className="text-center text-sm  md:text-xl lg:text-2xl">
+            Kerala, India
+          </p>
         </div>{" "}
-        <div className="flex gap-4">
+        <div className="flex gap-4 ">
           <a
             href="https://github.com/VinayakS404"
             target="_blank"
             rel="noreferrer"
+            className="shrink-0"
           >
-            <img src={githubIcon} alt="github icon" className="h-9" />
+            <img src={githubIcon} alt="github icon" className=" h-10  md:h-9" />
           </a>
           <a
             href="https://www.linkedin.com/in/vinayak-s-390319369/"
             target="_blank"
             rel="noreferrer"
+            className="shrink-0"
           >
-            <img src={linkedinIcon} alt="github icon" className="h-9" />
+            <img
+              src={linkedinIcon}
+              alt="github icon"
+              className="h-10  md:h-9"
+            />
           </a>
-          <img src={mailIcon} alt="github icon" className="h-9" />
+
+          <a className="shrink-0">
+            <img src={mailIcon} alt="github icon" className=" h-10  md:h-9" />
+          </a>
         </div>
-        <a href="">
-          <p>Download CV</p>
-        </a>
+        <div className="flex gap-10  " >
+          <a href="">
+            <p className="text-center   text-sm  md:text-xl lg:text-2xl">
+              Download CV
+            </p>
+          </a>
+          <div className="md:hidden flex items-center justify-center gap-1">
+            <img src={mapIcon} alt="pin icon" className=" h-3 md:h-5 " />
+            <p className="text-center text-sm  md:text-xl lg:text-2xl">
+              Kerala, India
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
 function NavBar() {
   return (
-    <nav className="   h-[30%]  w-full  flex justify-end  text-xl mt-[calc(100%-97%)] mr-50">
-      <ul className="flex gap-10">
-        <li>Home</li>
-        <li>Skills</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <>
+      <nav className="hidden md:flex h-[30%] w-full justify-end  text-xl mt-[calc(100%-97%)] mr-50 ">
+        <ul className="flex gap-10">
+          <li>Home</li>
+          <li>Skills</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div className=" flex md:hidden h-[20%] w-full">
+        <div className="h-15 text-[#83848b] bg-[#212223] w-full">working on it'</div>
+      </div>
+    </>
   );
 }
 export default NavBar;
