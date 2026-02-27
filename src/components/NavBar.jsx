91cfb0d7c4cf2ea87d1d2231770ea1cdfc58b7a8
Vinayak S <oreva.vinayak@gmail.com>
2026-02-28 03:36:07 +0530

    feat: add hamburgerIcon and add blue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
+import hamburgerIcon from "../assets/hamburger.png";
 function NavBar() {
   return (
     <>
-      <nav className="hidden md:flex h-[30%] w-full justify-end  text-xl mt-[calc(100%-97%)] mr-50 ">
+      <nav className="hidden md:flex  h-[30%] w-full justify-end  text-xl mt-[calc(100%-97%)] mr-50 ">
         <ul className="flex gap-10">
           <li>Home</li>
           <li>Skills</li>
@@ -10,8 +11,9 @@ function NavBar() {
           <li>Contact</li>
         </ul>
       </nav>
-      <div className=" flex md:hidden h-[20%] w-full">
-        <div className="h-15 text-[#83848b] bg-[#212223] w-full">working on it'</div>
+
+      <div className="md:hidden fixed top-0 left-0 w-full h-16 bg-[#202021]/80 backdrop-blur-2xl flex items-center justify-end pr-5 border-b border-[#313136] z-50">
+        <img className="h-7" src={hamburgerIcon} alt="hamburger icon" />
       </div>
     </>
   );
